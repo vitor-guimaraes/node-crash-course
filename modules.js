d6692ee3158e5f92ea8eb84fabc045872ba08c59
Vintidois <guimvitor@gmail.com>
2021-12-08 10:26:55 -0300

    exports v1

---
+++ modules.js
@@ -0,0 +1,8 @@
+const people = require('./people');
+console.log('peopele object:', people);
+
+const data = require('./people');
+console.log('data variable:', data);
+
+
+// console.log(data.people, data.ages);

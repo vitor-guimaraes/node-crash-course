e09bf9d4fc9d6bfe257fb3dd6149d8ecf505d7e0
Vintidois <guimvitor@gmail.com>
2021-12-08 14:45:04 -0300

    return html pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require ('http');
+const fs = require ('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -11,9 +12,21 @@ const server = http.createServer((req, res) => {
     //SET HEADER COM HTML
     res.setHeader('Content-type', 'text/html')
 
+    //SEND AN HTML FILE
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) { 
+            console.log(err);
+            res.end();        
+        } else {
+            // res.write(data);
+            // res.end();
+            res.end(data);
 
-    res.write('<p>hello in html</p>');
-    res.end();
+        }
+    })
+
+    // res.write('<p>hello in html</p>');
+    // res.end();
 });
 
 server.listen(3000, 'localhost', () => {

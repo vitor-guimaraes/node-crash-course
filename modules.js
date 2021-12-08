5aedffb1d68e6e3f5d26fd91a36c0c38c08a3fe1
Vintidois <guimvitor@gmail.com>
2021-12-08 10:32:49 -0300

    exports v2

--- modules.js
+++ modules.js
@@ -1,8 +1,14 @@
-const people = require('./people');
-console.log('peopele object:', people);
+// const people = require('./people');
+// console.log('peopele object:', people);
 
-const data = require('./people');
-console.log('data variable:', data);
-
-
+// const data = require('./people');
+// console.log('data variable:', data);
 // console.log(data.people, data.ages);
+
+
+const { people, ages } = require('./people');
+console.log(people, ages);
+
+
+const os = require('os');
+console.log(os.platform(), os.homedir());
--- people.js
+++ people.js
@@ -3,5 +3,9 @@ const ages = [20, 25, 30, 35];
 
 // console.log(people);
 
-module.exports = people;
+//module.exports = people;
 
+module.exports = {
+  people,
+  ages,
+}

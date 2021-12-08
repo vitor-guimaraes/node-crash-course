39581974c2a19993fb87658add2f4e2c69e20c03
Vintidois <guimvitor@gmail.com>
2021-12-08 14:22:04 -0300

    server listening

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require ('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //SET HEADER
+    // res.setHeader('Content-type', 'text/plain')
+    // res.write('hello');
+    // res.end();
+    
+    //SET HEADER COM HTML
+    res.setHeader('Content-type', 'text/html')
+
+
+    res.write('<p>hello in html</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {

a107315e681e79d5288464e99e7a42ffaa9031c1
Vintidois <guimvitor@gmail.com>
2021-12-08 11:57:45 -0300

    piping

--- files.js
+++ files.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 
-//read files
+//READ FILES
 
-// fs.readFile('./docs/blog.txt', (err, data) => {
-//     if(err) {
-//         console.log(err)
-//     }
-//     //console.log(data) //não le o conteudo do arquivo
-//     console.log(data.toString())
-// });
-
-
-//write files
-
-// fs.writeFile('./docs/blog.txt', 'hello, world', () => {
-//     console.log('file was written');
-// });
-
-// fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
-//     console.log('file was written');
-// });
+fs.readFile('./docs/blog.txt', (err, data) => {
+    if(err) {
+        console.log(err)
+    }
+    //console.log(data) //não le o conteudo do arquivo
+    console.log(data.toString())
+});
 
 
-//directories
+//WRITE FILES
+
+fs.writeFile('./docs/blog.txt', 'hello, world', () => {
+    console.log('file was written');
+});
+
+fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+    console.log('file was written');
+});
+
+
+//DIRECTORIES
 
 //1) CRIAR FOLDER
 //   fs.mkdir('./assets', (err) => {
@@ -45,21 +45,21 @@ const fs = require('fs');
 //     console.log('folder exists already!');
 
 //3) CRIAR FOLDER VERIFICANDO EXISTENCIA, APAGA SE JÁ EXISTE
-// if (!fs.existsSync ('./assets')) {
-//     fs.mkdir('./assets', (err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//     console.log ('folder created!');
-//     }); 
-// } else {
-//     fs.rmdir('./assets', (err) => {
-//         if (err) {
-//             console.log(err)
-//         }
-//     console.log ('folder deleted!')
-//     }); 
-// }
+if (!fs.existsSync ('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    console.log ('folder created!');
+    }); 
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+    console.log ('folder deleted!')
+    }); 
+}
 
 
 // deleting files
--- streams.js
+++ streams.js
@@ -2,10 +2,20 @@ const fs = require ('fs');
 
 // const readStream = fs.createReadStream('./docs/blog3.txt')
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8'})
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 
-readStream.on('data', (chunk) => {
-    console.log('----- NEW CHUNK -----');
-    //console.log(chunk.toString());
-    console.log(chunk);
-});
+//STREAM
+// readStream.on('data', (chunk) => {
+//     console.log('----- NEW CHUNK -----');
+//     //console.log(chunk.toString());
+//     console.log(chunk);
+
+//     //ESCRITA
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// });
+
+
+//PIPING
+readStream.pipe(writeStream);
